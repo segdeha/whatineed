a637b1cdba9955381967ebf041efb925bd26d3bf
K. Prasch <kieranprasch@gmail.com>
2016-06-29 15:44:46 -0700

    registering models in the django admin

--- web/admin.py
+++ web/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Thing, People, Purchase
 
-
-# Register your models here.
+admim.site.register(Thing)
+admim.site.register(People)
+admim.site.register(Purchase)
