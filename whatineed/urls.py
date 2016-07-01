43393e68253a606389e41278b4991240c627988a
Andrew Hedges <andrew@pdxcodeguild.com>
2016-07-01 16:52:54 -0700

    Enabled serving of static assets

--- whatineed/urls.py
+++ whatineed/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from api import urls
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(urls)),
     url(r'^accounts/', include('django.contrib.auth.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
