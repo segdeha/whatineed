cccf9432de032ab4101afa43200f855021bad109
Jeff Lombard <jeffl>
2016-07-01 15:13:39 -0700

    updated routes, and updated views, added comments

--- api/views.py
+++ api/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
-import json
-import datetime
+from django.http import JsonResponse,HttpResponse
+from django.db.models import lookups
+from web.models import Thing
+import datetime # not sure if this is needed
 
 # Create your views here.
 
-from django.http import JsonResponse,HttpResponse
-from web.models import Thing
-from django.db.models import lookups
-
+# Given a barcode number return object with information about that product
 def barcode(request, barcode_number):
     try:
-        # Return Success from jsonapi.org/format
+        # Return Success format from jsonapi.org/format
         data = dict(Thing.objects.filter(barcode = barcode_number).values().first())
         json_object = {'data':data, 'self': '/api/thing/' + barcode_number}
     except TypeError:
-        # Return Error from jsonapi.org/format
+        # Return Error format from jsonapi.org/format
         data = None
         title = 'No Item Found'
         detail = 'There was no item in our records that matched the barcode you supplied.'
@@ -22,14 +21,66 @@ def barcode(request, barcode_number):
         json_object = {'errors' : error, 'data': data}
     return JsonResponse(json_object)
 
+# Given a username generate a list or dictionary of objects
 def people_need(request,username):
-    pass
+    data = None #define data here!
+    # /people/id/list/need
+    json_object = {'data':data, 'self': '/people/'+ username +'/list/need' + barcode_number}
+    try:
+        # Look up username
+        # If the username is found, try looking up needs:
+        pass
+        try:
+            # Look up history
+            data = None # Replace
+            json_object = {'data':data, 'self': '/api/thing/' + barcode_number}
+            pass
+        except TypeError:
+            # Needs Not Found
+            title = 'No items found'
+            detail = 'User does not need any items'
+            error = {'status':"404 Not Found", 'title':title,'detail':detail}
+            json_object = {'errors': error, 'data' : data }
+            return JsonResponse(json_object)
+    except TypeError:
+        # If you are here username look up failed
+        title = 'Username Not Found'
+        detail = 'There was no username in our records that matches that request'
+        error = {'status':"404 Not Found", 'title':title,'detail':detail}
+        json_object = {'errors': error, 'data' : data }
+        return JsonResponse(json_object)
 
+# Given a username generate a list or dictionary of objects
 def people_done(request,username):
-    pass
+    data = None #define data here!
+    # /people/id/list/need
+    json_object = {'data':data, 'self': '/people/'+ username +'/list/done' + barcode_number}
+    try:
+        # Look up username
+        # If the username is found, try looking up needs:
+        pass
+        try:
+            # Look up history
+            pass
+        except TypeError:
+            # done Not Found
+            title = 'No items found'
+            detail = 'User has not finished any items'
+            error = {'status':"404 Not Found", 'title':title,'detail':detail}
+            json_object = {'errors': error, 'data' : data }
+            return JsonResponse(json_object)
+    except TypeError:
+        # If you are here username look up failed
+        title = 'Username Not Found'
+        detail = 'There was no username in our records that matches that request'
+        error = {'status':"404 Not Found", 'title':title,'detail':detail}
+        json_object = {'errors': error, 'data' : data }
+        return JsonResponse(json_object)
 
+#
 def people_usage(request,username,barcode_number):
     pass
 
+#
 def people_purchase(request,username,barcode_number):
     pass
