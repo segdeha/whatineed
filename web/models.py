bfaa2ee63c5ee9ea3292f42c8f1be7779290fc92
lacunahag <jasmine.yhumbert@gmail.com>
2016-07-01 11:36:46 -0700

    this is broken

--- web/models.py
+++ web/models.py
@@ -4,6 +4,9 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.core.exceptions import ValidationError
 from datetime import timedelta
 
+import statistics as stats
+import numpy as np
+
 # Create your models here.
 
 
@@ -42,7 +45,26 @@ class Purchase(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     purchase_date = models.DateTimeField(auto_now=False, blank=True, null=True)
     consumed_date = models.DateTimeField(auto_now=False, blank=True, null=True)
+    predicted_replace_days = models.PositiveSmallIntegerField(blank=True)
 
     def duration(self):
         duration = self.consumed_time - self.purchase_time
         return duration
+
+    def relevant_mean(user_predict, *args): # returns mean of data it deems relevant
+        if isinstance(args[0], list):
+            collected_data = args[0]
+        else:
+            collected_data = list(args)
+        # add the initial user prediction to the beginning of our actual data
+        all_data = [user_predict] + collected_data
+        # user_predict and all args should be ints
+        if not all(isinstance(item, int) for item in all_data):
+                raise TypeError("predict() needs positive ints!")
+                # is it worth checking for negative ints?
+        if len(all_data) < 4: # if there's only 4 datapoints use all of them
+            return int(stats.mean(all_data))
+        elif len(all_data) < 10: # 10 is very arbitrary
+            return int(np.percentile(all_data, 10)) # np.percentile excludes data outside the 80th percentile
+        else: # only use the last ten data points
+            return int(np.percentile(args[-10:], 10))
