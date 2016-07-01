352f3cf02d8945ceea1e006eba226dc744966919
Jeff Lombard <jeffl>
2016-07-01 15:46:28 -0700

    updated urls comments

--- api/urls.py
+++ api/urls.py
@@ -8,15 +8,18 @@ urlpatterns = [
     # /thing/barcodenumber -> json object for unique thing
     url(r'thing/(?P<barcode_number>[0-9]+)/', views.barcode ),
 
-    # /people/id/list/need
+    # /people/<username>/list/need
     url(r'people/(?P<username>\w+)/list/need', views.people_need),
 
-    # /people/id/history/done
+    # /people/<username>/history/done
     url(r'people/(?P<username>\w+)/list/done', views.people_done),
 
-    # /people/id/history/purchased
+    # /people/<username>/history/purchased
+    # this may have to be changed because accessing this link will update purchased history
     url(r'people/(?P<username>\w+)/history/(?P<barcode_number>[0-9]+)/purchased', views.people_purchase),
 
-    # /people/id/history/usage
+    # /people/<username>/history/usage
+    # this may have to be changed because accessing this link will update usage history
+    # need to look into best practices, because it seems to me you don't want a plain url being able to edit the database.
     url(r'people/(?P<username>\w+)/history/(?P<barcode_number>[0-9]+)/usage', views.people_usage),
 ]
